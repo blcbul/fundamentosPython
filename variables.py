6e10f2246a7f2d535bc90843310974a99f4272c3
Ismael Otamendi Sanchez <ismaotsss@gmail.com>
2023-06-09 16:20:41 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+#Python es No tipado
+
+name = "Ismael"
+last_name = "Otamendi"
+age = 19
+height = 1.77
+is_student = False
+groups = ["3A","3B","3C"]
+
+# function type
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(groups))
