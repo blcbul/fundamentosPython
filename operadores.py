56d6bd9f32fc02030cf4add2c0dde36a7827d39f
Ismael Otamendi Sanchez <ismaotsss@gmail.com>
2023-06-09 16:52:17 -0600

    04 Operadores

---
+++ operadores.py
@@ -0,0 +1,19 @@
+# operadores matematicos
+# + - * %
+# / División float
+# // División entera
+# ** Potencia
+print(12+12)
+print(12-12)
+print(12*12)
+print(12%12)
+print(10/3)
+print(10//3)
+print(2**16)
+
+# Operadores para strings
+# + para concatenar
+# * para replicar
+print("-------"*4)
+print("Hola " + "Mundo!")
+print("Hola " * 3)
