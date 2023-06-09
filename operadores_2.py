9eaf3de95c838a873791b1eb7696d98aa5d30e65
Ismael Otamendi Sanchez <ismaotsss@gmail.com>
2023-06-09 16:59:19 -0600

    05 Operadores Logicos y Booleanos

---
+++ operadores_2.py
@@ -0,0 +1,18 @@
+# Operadores booleanos
+# == (igualdad)
+print(1==1)
+# != (distinto)
+print(1!=1)
+# > (Mayor que)
+print(1>1)
+# < (Menor que)
+print(1<1)
+# Operadores Logicos
+# and
+print(True and True)
+print(True and True)
+# or
+print(True or True)
+print(False or False)
+# not
+print(not True)
